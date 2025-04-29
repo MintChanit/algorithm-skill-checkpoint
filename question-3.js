d5f98abb3a7774501aa960c72353d5e2504a6f63
MINT <mint_chanit@hotmail.com>
2025-04-29 10:44:41 +0700

    feat: add answer to algorithm skill checkpoint

--- question-3.js
+++ question-3.js
@@ -1,20 +1,21 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
 Answer:
-
+    O(n) หรือ Linear Time เพราะ Function findStudentById มีการใช้ Linear Search Algorithm
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
 Answer:
-
+    O(log n) หรือ Logarithm Time เพราะ Function findProductPrice มีการใช้ Binary Search Algorithm
 
 */
 
 /* 
 Which function is more efficient and why?
 Answer:
-
+    Function findProductPrice มีประสิทธิภาพมากกว่า Function findStudentById 
+    เพราะ Function findProductPrice มี Big O เป็น O(log n) หรือ Logarithm Time ซึ่งมี Time Complexity หรือจำนวนรอบของการทำงานน้อยกว่า Function findStudentById ที่มี Big O เป็น O(n) หรือ Linear Time
 
 */
--- question-5.js
+++ question-5.js
@@ -1,6 +1,6 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
 Answer:
-
+    o(n^2) หรือ Quadratic Time เพราะ Function sortProductsByPrice มีการใช้ Bubble Sort Algorithm 
 
 */
